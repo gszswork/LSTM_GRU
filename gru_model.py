ecbf33e9eeff4d9ccaeca36d06ab68b95bbd616d
Daniel <beishazimileyan@outlook.com>
2021-04-29 15:50:22 +1000

    GPU training, bugs fixed

--- gru_model.py
+++ gru_model.py
@@ -1,10 +1,11 @@
 import torch
 import torch.nn as nn
 
-device = 'cuda'
+device = 'cpu'
+
 
 class LSTM_GRU(nn.Module):
-    def __init__(self, word_vec_dim=25, LSTM_dim=128, GRU_dim=256,classes=2):
+    def __init__(self, word_vec_dim=25, LSTM_dim=128, GRU_dim=256, classes=2):
         """
         :param word_vec_dim: The dimension of word vector
         :param classes: The number of classes
@@ -17,28 +18,38 @@ class LSTM_GRU(nn.Module):
         self.FC = nn.Linear(GRU_dim, classes)
         self.solfmax = nn.Softmax(dim=-1)
 
-    def forward(self, input_tree):
-
-        output_list = []
-        LSTM_hidden = (torch.zeros(size=(1, 1, self.LSTM_dim)),
+    def init_hidden(self):
+        LSTM_hidden  = (torch.zeros(size=(1, 1, self.LSTM_dim)),
                        torch.zeros(size=(1, 1, self.LSTM_dim))) # hidden_dim: (num_layers*num_direction,  batch, hidden_size)
+        GRU_hidden = torch.zeros(size=(1, 1, self.GRU_dim))
+        return LSTM_hidden, GRU_hidden
+
+    def forward(self, input_tree, LSTM_hidden, GRU_hidden):
+        output_list = []  # Each treee keeps a output_list to hold all sentence outputs.
+
+        ############################### sentence LSTM block ####################################################
+        LSTM_hidden = (LSTM_hidden[0].to(device), LSTM_hidden[1].to(device))
         for sentence in input_tree:
             # sentence.shape: [seq_length, input_size]
             sentence_input = torch.tensor(sentence)
-            sentence_input = sentence_input.unsqueeze(dim=1)
-            output, LSTM_hidden = self.sentence_LSTM(sentence_input, LSTM_hidden) # input_dim: (seq_length, batch, input_size)
-            output_list.append(output)
+            sentence_input = sentence_input.unsqueeze(dim=1).to(device)
+            output, LSTM_hidden = self.sentence_LSTM(sentence_input,
+                                                     LSTM_hidden)  # input_dim: (seq_length, batch, input_size)
+            # print(LSTM_hidden[0].shape, LSTM_hidden[1].shape)
+            output_list.append(output[-1])
+        # Up to now, tree_represent's shape : [sentence_num, word_embedding_dim]
+
+        ############################## tree GRU block ########################################################
         tree_represent = torch.cat(output_list, dim=0)
-
-        GRU_hidden = torch.zeros(size=(1,1, self.GRU_dim))
+        tree_represent = tree_represent.unsqueeze(dim=1)  # [sentence_num, 1, word_embedding_dim]
+        # print(tree_represent.shape)
+        GRU_hidden = GRU_hidden.to(device)
         GRU_output, GRU_hidden = self.tree_GRU(tree_represent, GRU_hidden)
+        # print(GRU_output[-1].shape)
+        FC_output = self.FC(GRU_output[-1])  # [1, GRU_hidden_dim]
 
-        FC_output = self.FC(GRU_output)
-
-        res =  self.solfmax(FC_output)
-        return res.squeeze(dim=0).squeeze(dim=0)
-
-
+        res = self.solfmax(FC_output)
+        return res.squeeze(dim=0), LSTM_hidden, GRU_hidden
 
 
 # The input format of LSTM_GRU:
@@ -54,3 +65,48 @@ Each sentence contains several word vectors of same dimension
 sentence = [vector1, vector2, ... vectorn] 
 
 '''
+
+
+class GRU(nn.Module):
+    def __init__(self, GRU_dim=256, classes=2):
+        """
+        :param word_vec_dim: The dimension of word vector
+        :param classes: The number of classes
+        """
+        super(GRU, self).__init__()
+        self.GRU_dim = GRU_dim
+        self.tree_GRU = nn.GRU(input_size=256, hidden_size=GRU_dim)
+        self.FC = nn.Linear(GRU_dim, classes)
+        self.solfmax = nn.Softmax(dim=-1)
+
+    def forward(self, tree_represent, hidden=None):
+        # input : [3, 1, self.GRU_dim]
+        GRU_hidden = hidden
+        GRU_output, GRU_hidden = self.tree_GRU(tree_represent, GRU_hidden)
+        FC_output = self.FC(GRU_output[-1])  # [1, GRU_hidden_dim]
+        return FC_output.squeeze(dim=0), GRU_hidden
+
+
+if __name__ == "__main__":
+    input = torch.randn(3, 1, 256)
+    target = torch.randn(2)
+    hidden = torch.zeros(size=(1, 1, 256))
+    model = GRU(GRU_dim=256,
+                classes=2).to(device)
+    model_optimizer = torch.optim.SGD(
+        # params=filter(lambda p: p.requires_grad, model.parameters()),
+        params=model.parameters(),
+        momentum=0.9,
+        lr=0.1
+    )
+
+    loss_func = nn.MSELoss(reduction='sum')
+    for i in range(10):
+        hidden = hidden.data
+        pred, hidden = model(input, hidden)
+
+        loss = loss_func(pred, target)
+        loss.backward()
+        model_optimizer.step()
+
+
--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 import torch, torch.nn as nn
 import gru_model
+import pickle
+from tqdm import tqdm
+from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score
+import numpy as np
 device = 'cpu'
 
 word_vector_dim = 25
 LSTM_dim = 128
-GRU_dim = 256
+GRU_dim = 32
 classes = 2
 epochs = 100
 lr = 0.005
 
-import pickle
+
 dirname = 'data/'
-def load_obj(name ):
-    with open( dirname + name + '.pkl', 'rb') as f:
+
+
+def load_obj(name):
+    with open(dirname + name + '.pkl', 'rb') as f:
         return pickle.load(f)
+
 
 model = gru_model.LSTM_GRU(word_vec_dim=word_vector_dim,
                            LSTM_dim=LSTM_dim,
@@ -24,39 +31,62 @@ print('loaded_model, putting into ', device)
 train_data = load_obj('train_set')
 dev_data = load_obj('dev_set')
 test_data = load_obj('test_set')
-
+train_label = load_obj('train_label')
+dev_label = load_obj('dev_label')
 model_optimizer = torch.optim.SGD(
-	# params=filter(lambda p: p.requires_grad, model.parameters()),
-	params=model.parameters(),
-	momentum=0.9,
-	lr=lr
+    # params=filter(lambda p: p.requires_grad, model.parameters()),
+    params=model.parameters(),
+    momentum=0.9,
+    lr=lr
 )
 
 loss_func = nn.MSELoss(reduction='sum')
 
-tree1 = [[[1.0 ,1.0 ,1.0 ,1.0 ,1.0, 1.0 ,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.00,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.,1.0,1.0,1.0]]]
-train_data = [tree1, tree1, tree1]
-train_label = [[0.0, 1.0], [1.0, 0.0], [0.0, 0.0]]
+# train_data = [tree1, tree1, tree1]
+# train_label = [[0.0, 1.0], [1.0, 0.0], [0.0, 0.0]]
+# TODO handle the hidden 问题出在hidden上面，添加了hidden以后就不行了
 
-for iter in range(epochs):
-    for idx in range(len(train_data)):
-        pred = model(train_data[idx])
-        target = torch.FloatTensor(train_label[idx])
+LSTM_hidden, GRU_hidden = model.init_hidden()
+
+for iteration in range(epochs):
+    train_loss = []
+    for idx in tqdm(range(len(train_data))):
+        if idx == 3126:
+            continue    # The 3126th dataset is not complete
+        #hidden = (tuple([each.data for each in hidden[0]]), [each.data for each in hidden[1]])
+        LSTM_hidden = tuple([e.data for e in LSTM_hidden])
+        GRU_hidden = GRU_hidden.data                                #
+        pred, LSTM_hidden, GRU_hidden = model(train_data[idx], LSTM_hidden, GRU_hidden)
+        # print(pred.shape)
+        target = torch.FloatTensor(train_label[idx]).to(device)
         loss = loss_func(pred, target)
+        train_loss.append(loss.data.cpu().numpy())
         model_optimizer.zero_grad()
-        loss.backward()
+        loss.backward(retain_graph=True)
 
-        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=5.0)
+        #torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=5.0)      #梯度裁剪，暂且不用
         model_optimizer.step()
-    if epochs/5==0:
-        with torch.no_grad:
-            for idx in range(len(dev_data)):
-                pred = model(dev_data[idx])
-                
+    print('epoch={}: loss={:.6f}s'.format(iteration, np.mean(train_loss)))
+    with torch.no_grad():
+        pred_list = []
+        for idx in range(len(dev_data)):
+            pred, _, _ = model(dev_data[idx], LSTM_hidden, GRU_hidden)
+            pred = pred.to('cpu').numpy()
+            if pred[0] > pred[1]:
+                new_pred = 1
+            else:
+                new_pred = 0
+            pred_list.append(new_pred)
+        #print(dev_label[0], pred_list[0])
+        print()
+        acc = accuracy_score(dev_label, pred_list)
+        f1 = f1_score(dev_label, pred_list, average='macro')
+        p = precision_score(dev_label, pred_list, average='macro',zero_division=True)
+        r = recall_score(dev_label, pred_list, average='macro')
+        print('acc:', acc, ', F1:',f1, ' ,Precision:', p, ' ,Recall:', r)
+    torch.save(model.state_dict(), 'check_points/model_'+str(iteration)+'.pt')
 
-
-
-
+torch.save(model.state_dict(), 'check_points/final_model.pt')
 
 '''
 
